d17ed349d5d1592f858a635688b819c5bf54fba4
Robosid <robo_sid@yahoo.co.uk>
2018-03-19 19:59:29 +0530

    Revised

--- Test_Scripts_Unstable/maneuver.py
+++ Test_Scripts_Unstable/maneuver.py
@@ -12,3 +12,43 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 '''
+
+from dronekit import connect, VehicleMode, LocationGlobalRelative
+import time
+
+#-- Connect to the vehicle
+import argparse
+parser = argparse.ArgumentParser(description='commands')
+parser.add_argument('--connect', default='/dev/ttyS0')
+args = parser.parse_args()
+
+connection_string = args.connect
+
+
+print("Connection to the vehicle on %s"%connection_string)
+vehicle = connect(args.connect, baud=921600, wait_ready=True)
+
+#-- Define the function for takeoff
+def arm_and_takeoff(tgt_altitude):
+    print("Arming motors")
+    
+    while not vehicle.is_armable:
+        time.sleep(1)
+        
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+    
+    while not vehicle.armed: time.sleep(1)
+    
+    print("Takeoff")
+    vehicle.simple_takeoff(tgt_altitude)
+    
+    #-- wait to reach the target altitude
+    while True:
+        altitude = vehicle.location.global_relative_frame.alt
+        
+        if altitude >= tgt_altitude -1:
+            print("Altitude reached")
+            break
+            
+        time.sleep(1)
